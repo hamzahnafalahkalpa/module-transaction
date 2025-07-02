396081a3660b96e955c75aa459e24a54c360adc5
Hamzah <hamzahnafalah@gmail.com>
2025-07-02 14:58:47 +0700

    modules

--- src/Contracts/Schemas/ReportTransaction.php
+++ src/Contracts/Schemas/ReportTransaction.php
@@ -2,11 +2,4 @@
 
 namespace Hanafalah\ModuleTransaction\Contracts\Schemas;
 
-use Hanafalah\LaravelSupport\Contracts\Data\PaginateData;
-use Illuminate\Pagination\LengthAwarePaginator;
-
-interface ReportTransaction extends Transaction
-{
-    public function prepareViewTransactionReportPaginate(mixed $cache_reference_type = null, ?array $morphs = null, PaginateData $paginate_dto): LengthAwarePaginator;
-    public function viewTransactionReportPaginate(mixed $cache_reference_type = null, ?array $morphs = null, ? PaginateData $paginate_dto = null): array;
-}
+interface ReportTransaction extends Transaction{}

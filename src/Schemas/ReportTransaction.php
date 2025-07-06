8e53803697a6a990f60052d8127bee5472bcfd58
Hamzah <hamzahnafalah@gmail.com>
2025-07-07 01:11:56 +0700

    update modules

--- src/Schemas/ReportTransaction.php
+++ src/Schemas/ReportTransaction.php
@@ -2,7 +2,7 @@
 
 namespace Hanafalah\ModuleTransaction\Schemas;
 
-use Hanafalah\ModulePayment\Contracts\Schemas\ReportTransaction as SchemasReportTransaction;
+use Hanafalah\ModuleTransaction\Contracts\Schemas\ReportTransaction as SchemasReportTransaction;
 use Illuminate\Database\Eloquent\Builder;
 
 class ReportTransaction extends Transaction implements SchemasReportTransaction
